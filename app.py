241c292abbb5f3c54c0c870acadf009daef7bbee
louiedweck <dweck.louis@gmail.com>
2020-04-06 12:14:40 -0400

    second question

--- app.py
+++ app.py
@@ -13,4 +13,18 @@ def find_indicies(nums, target):
     return []
 
 
+def reverse(x: int) -> int:
+    if x > 0:  # handle positive numbers
+        a = int(str(x)[::-1])
+    if x <= 0:  # handle negative numbers
+        a = -1 * int(str(x*-1)[::-1])
+    min = -2**31
+    max = 2**31 - 1
+    if a not in range(min, max):
+        return 0
+    else:
+        return a
+
+
 print(find_indicies(nums, 10))
+print(reverse(98457))
