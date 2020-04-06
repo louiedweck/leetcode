b51a6b1ed3f902ac1fc187feca13b70ac4b7518f
louiedweck <dweck.louis@gmail.com>
2020-04-06 12:24:24 -0400

    second problem

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ nums = [0, 1, 2, 6, 8, 9, 10]
 
 
 def find_indicies(nums, target):
-    seenit = set()
     seen = {}
     for i, v in enumerate(nums):
-        remaining = target - v
-        if remaining in seen:
-            return [seen[remaining], i]
+        # looking for complimentary number to target
+        compliment = target - v
+        if compliment in seen:
+            # we have our set
+            return [seen[compliment], i]
         seen[v] = i
-        seenit.add(i)
     return []
 
 
@@ -18,12 +18,7 @@ def reverse(x: int) -> int:
         a = int(str(x)[::-1])
     if x <= 0:  # handle negative numbers
         a = -1 * int(str(x*-1)[::-1])
-    min = -2**31
-    max = 2**31 - 1
-    if a not in range(min, max):
-        return 0
-    else:
-        return a
+    return a
 
 
 print(find_indicies(nums, 10))
